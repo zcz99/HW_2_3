62ecb91df917c1fc3b9ab2b54402374695f9ca47
zcz99 <zvezdncot2017@gmail.com>
2025-03-21 16:06:06 +0300

    I has finished the app

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,35 +1,76 @@
 import { useState } from 'react';
 import { data } from './data.js';
 import styles from './App.module.css';
+import { useEffect } from 'react';
+
+let NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '=', 'C'];
 
 function App() {
 	const [operand1, setOperand1] = useState('');
 	const [operand2, setOperand2] = useState('');
 	const [operator, setOperator] = useState('');
+	const [equally, setEqually] = useState(0);
 
-	function Cancel() {
+	console.log('operand1', operand1);
+	console.log('operand2', operand2);
+
+	function cancel() {
 		setOperand1('');
 		setOperand2('');
 		setOperator('');
+		setEqually(0);
 	}
 
-	function Plus() {
+	function plus() {
 		setOperator('+');
+		setEqually(0);
 	}
-	function Minus() {
+	function minus() {
 		setOperator('-');
+		setEqually(0);
+	}
+	function numberKey(event) {
+		if (operator === '') {
+			setOperand1(`${operand1}${event.target.innerText}`);
+		} else {
+			setOperand2(`${operand2}${event.target.innerText}`);
+		}
+		setEqually(0);
+	}
+	function equallyNumber() {
+		setEqually(1);
 	}
 
-	let NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '=', 'C'];
+	// let displayExpression = null
+
 	return (
 		<>
-			<div className={styles.display}></div>
+			<div
+				className={
+					equally ? `${styles.display} ${styles.equally}` : styles.display
+				}
+			>
+				{equally
+					? eval(`${operand1} ${operator} ${operand2}`)
+					: `${operand1} ${operator} ${operand2}`}
+			</div>
 
 			<div>
 				<ul className={styles.digital_panel}>
 					{NUMS.map((item, index) => (
 						<li className="" key={index}>
-							<button className={styles.button} onClick={''}>
+							<button
+								disabled={equally ? index <= 9 && true : false}
+								value={item}
+								className={styles.button}
+								onClick={
+									(index <= 9 && numberKey) ||
+									(index === 10 && plus) ||
+									(index === 11 && minus) ||
+									(index === 12 && equallyNumber) ||
+									(index === 13 && cancel)
+								}
+							>
 								{item}
 							</button>
 						</li>
